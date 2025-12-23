3a319933192f394352e3806b59e8f002717583ba
AungNyanHtet18 <aungnyanh81@gmail.com>
2025-12-23 23:59:28 +0630

    complete for specifying jwt token properties

--- jobportal-backend/src/main/java/com/dev/anh/job/model/service/JwtTokenProvider.java
+++ jobportal-backend/src/main/java/com/dev/anh/job/model/service/JwtTokenProvider.java
@@ -2,6 +2,7 @@ package com.dev.anh.job.model.service;
 
 import javax.crypto.SecretKey;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
@@ -12,16 +13,29 @@ public class JwtTokenProvider {
 
 	private SecretKey secret = Jwts.SIG.HS256.key().build();
 	
+	@Value("${app.jwt.issuer}")
+	private String issuer;
+	@Value("${app.jwt.access-life}")
+	private int accessLife;
+	@Value("${app.jwt.refresh-life}")
+	private int refreshLife;
+	
+	
 	public String generateAccessToken(Authentication authentication) {
-		
 		return null;
 	}
 	
 	public String generateRefreshToken(Authentication authentication) {
-		
 		return null;
 	}
 
+
+	public Authentication parseAccessToken(String jwtToken) {
+		
+		
+		return null;
+	}
+	
 	public Authentication parseRefreshToken(String token) {
 		
 		
