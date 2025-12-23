6c3c5d964a319e5928dbfe76f68cd1374e5609fa
AungNyanHtet18 <aungnyanh81@gmail.com>
2025-12-24 00:25:28 +0630

    complete for writing jwt token provider

--- jobportal-backend/src/main/java/com/dev/anh/job/model/service/JwtTokenProvider.java
+++ jobportal-backend/src/main/java/com/dev/anh/job/model/service/JwtTokenProvider.java
@@ -1,10 +1,19 @@
 package com.dev.anh.job.model.service;
 
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.stream.Collectors;
+
 import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
+
+import com.dev.anh.job.model.consts.TokenType;
 
 import io.jsonwebtoken.Jwts;
 
@@ -20,27 +29,64 @@ public class JwtTokenProvider {
 	@Value("${app.jwt.refresh-life}")
 	private int refreshLife;
 	
-	
 	public String generateAccessToken(Authentication authentication) {
-		return null;
-	}
-	
-	public String generateRefreshToken(Authentication authentication) {
-		return null;
+		return generate(authentication, TokenType.Access);
 	}
 
+	public String generateRefreshToken(Authentication authentication) {
+		return generate(authentication, TokenType.Refresh);
+	}
 
 	public Authentication parseAccessToken(String jwtToken) {
-		
-		
-		return null;
+		return parse(jwtToken, TokenType.Access);
 	}
 	
-	public Authentication parseRefreshToken(String token) {
+	public Authentication parseRefreshToken(String jwtToken) {
+		return parse(jwtToken, TokenType.Refresh);
+	}
+	
+	
+	private String generate(Authentication authentication, TokenType type) {
+		var issuseAt = new Date();
+		var calendar = Calendar.getInstance();
+		calendar.setTime(issuseAt);
 		
+		if(type == TokenType.Access) {
+			 calendar.add(Calendar.MINUTE, accessLife);
+		}else if(type == TokenType.Refresh) { 
+			calendar.add(Calendar.MINUTE, refreshLife);
+		}
 		
-		return null;
+		return Jwts.builder()
+				   .subject(authentication.getName())
+				   .claim("role", authentication.getAuthorities().stream()
+						   .map(a -> a.getAuthority()).collect(Collectors.joining(",")))
+				   .claim("type", type.name())
+				   .signWith(secret)
+				   .issuer(issuer)
+				   .issuedAt(issuseAt)
+				   .expiration(calendar.getTime())
+				   .compact();
+	}
+	
+	
+	private Authentication parse(String jwtToken, TokenType type) {
+		
+		var payLoad = Jwts.parser()
+						  .verifyWith(secret)
+						  .requireIssuer(issuer)
+						  .require("type", type.name())
+						  .build()
+						  .parseSignedClaims(jwtToken).getPayload();
+		
+		  var username = payLoad.getSubject();
+		  
+		  var roles = Arrays.stream(payLoad.get("role", String.class)
+				  			.split(","))
+				  			.map(a -> new SimpleGrantedAuthority(a))
+				  			.toList();
+		  
+		  return UsernamePasswordAuthenticationToken.authenticated(username, null, roles);
 	}
 
-	
 }
